5095fc6df6443b9d7ff784a2b5c129f2f3325c6f
Sonny Mattera <smattera@hotmail.com>
2024-05-13 12:26:59 -0500

    chore: Update Contact component UI and form handling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,15 +3,28 @@ import { useForm, ValidationError } from "@formspree/react";
 export default function Contact() {
   const [state, handleSubmit] = useForm("xjvnzowg");
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+    return (
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <div className="pb-22">
+            <h2 className="text-xl font-semibold leading-7 text-gray-900">
+              Contact Us
+            </h2>
+            <p className="mt-1 text-base leading-6 text-gray-600">
+              Thanks for reaching out! We'll get back to you soon.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
   }
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 sm:py-32 lg:px-8">
       <div className="mx-auto max-w-2xl">
         <form onSubmit={handleSubmit}>
           <div className="space-y-12">
-            <div className="border-b border-gray-900/10 pb-22">
-              <h2 className="text-base font-semibold leading-7 text-gray-900">
+            <div className="pb-22">
+              <h2 className="text-xl font-semibold leading-7 text-gray-900">
                 Contact Us
               </h2>
               <p className="mt-1 text-sm leading-6 text-gray-600">
@@ -33,7 +46,7 @@ export default function Contact() {
                         name="name"
                         id="name"
                         autoComplete="name"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         placeholder="Please enter name"
                       />
                       <ValidationError
@@ -71,7 +84,7 @@ export default function Contact() {
                   </div>
                 </div>
 
-                <div className="sm:col-span-6">
+                <div className="col-span-full">
                   <label
                     htmlFor="message"
                     className="block text-sm font-medium leading-6 text-gray-900"
@@ -82,26 +95,31 @@ export default function Contact() {
                     <textarea
                       id="message"
                       name="message"
-                      rows={4}
-                      className="block w-full border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                      placeholder="Please enter message"
-                    ></textarea>
+                      rows={3}
+                      className="block w-full rounded-md border-0 py-1.5 min-h-8 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      defaultValue={""}
+                    />
                     <ValidationError
                       prefix="Message"
                       field="message"
                       errors={state.errors}
                     />
                   </div>
+                  <p className="mt-3 text-sm leading-6 text-gray-600">
+                    Write a few sentences about your project.
+                  </p>
                 </div>
 
-                <div className="mt-6 flex items-center justify-end gap-x-6">
-                  <button
-                    type="submit"
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    disabled={state.submitting}
-                  >
-                    Submit
-                  </button>
+                <div className="col-span-full">
+                  <div className="mt-2 flex items-center">
+                    <button
+                      type="submit"
+                      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      disabled={state.submitting}
+                    >
+                      Submit
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
