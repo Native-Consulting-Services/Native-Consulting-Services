ed9f9a389fcae4d1a366477691ee3ba1cb62ba54
Sonny Mattera <smattera@hotmail.com>
2024-05-18 20:37:20 -0500

    Update background color of the landing page to indigo

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Switch, SwitchGroup, Label } from "@headlessui/react";
 import { useForm } from "@formspree/react";
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function Contact() {
   const [state, handleSubmit] = useForm("xjvnzowg");
-  const [agreed, setAgreed] = useState(false);
 
   return (
-    <div
-      className=" relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8"
-      id="Contact"
-    >
+    <div className=" relative isolate px-6 py-24 sm:py-32 lg:px-8" id="Contact">
       {state.succeeded ? (
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -117,24 +106,6 @@ export default function Contact() {
                   Phone number
                 </label>
                 <div className="relative mt-2.5">
-                  <div className="absolute inset-y-0 left-0 flex items-center">
-                    <label htmlFor="country" className="sr-only">
-                      Country
-                    </label>
-                    <select
-                      id="country"
-                      name="country"
-                      className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                    >
-                      <option>US</option>
-                      <option>CA</option>
-                      <option>EU</option>
-                    </select>
-                    <ChevronDownIcon
-                      className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </div>
                   <input
                     type="tel"
                     name="phone-number"
@@ -161,34 +132,6 @@ export default function Contact() {
                   />
                 </div>
               </div>
-              <SwitchGroup as="div" className="flex gap-x-4 sm:col-span-2">
-                <div className="flex h-6 items-center">
-                  <Switch
-                    checked={agreed}
-                    onChange={setAgreed}
-                    className={classNames(
-                      agreed ? "bg-indigo-600" : "bg-gray-200",
-                      "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    )}
-                  >
-                    <span className="sr-only">Agree to policies</span>
-                    <span
-                      aria-hidden="true"
-                      className={classNames(
-                        agreed ? "translate-x-3.5" : "translate-x-0",
-                        "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                      )}
-                    />
-                  </Switch>
-                </div>
-                <Label className="text-sm leading-6 text-gray-600">
-                  By selecting this, you agree to our{" "}
-                  <a href="#" className="font-semibold text-indigo-600">
-                    privacy&nbsp;policy
-                  </a>
-                  .
-                </Label>
-              </SwitchGroup>
             </div>
             <div className="mt-10">
               <button
