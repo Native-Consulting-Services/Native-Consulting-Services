3fff2cff18b5ee5dbbe4111c92b623bcf152a8aa
Sonny Mattera <smattera@hotmail.com>
2024-05-21 13:46:57 -0500

    chore: Update postcss.config.js and tailwind.config.js

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import { Config } from "tailwindcss";
+import forms from "@tailwindcss/forms";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [forms],
+};
+
+export default config;
